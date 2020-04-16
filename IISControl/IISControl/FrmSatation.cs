822f64381999c9e62c57a4600c9c24513b82b883
chenzhaojie <15298506403@163.com>
2020-04-16 23:02:39 +0800

    测试

--- IISControl/IISControl/FrmSatation.cs
+++ IISControl/IISControl/FrmSatation.cs
@@ -25,7 +25,6 @@ namespace IISControl
         private string poolNmae = "";
         List<StationDetail> stationList = null;
 
-
         public FrmSatation(string _ip)
         {
             InitializeComponent();
