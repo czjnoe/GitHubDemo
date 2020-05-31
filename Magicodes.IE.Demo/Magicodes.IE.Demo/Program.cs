e9405fb9d930515184e6ced5676ecbd2f8638ebc
chenzhaojie <15298506403@163.com>
2020-05-31 22:47:03 +0800

    1111

---
+++ Magicodes.IE.Demo/Magicodes.IE.Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicodes.IE.Demo
+{
+    /// <summary>
+    /// Magicodes.IE
+    /// Github：https://github.com/dotnetcore/Magicodes.IE
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
