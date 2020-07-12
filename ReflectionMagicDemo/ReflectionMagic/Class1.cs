98badbdb16013f1d13657cc73de5093ebf9eb1ac
chenzhaojie <15298506403@163.com>
2020-07-12 17:08:04 +0800

    ReflectionMagic

---
+++ ReflectionMagicDemo/ReflectionMagic/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionMagicLibrary
+{
+    public class Foo1
+    {
+        private Foo2 GetOtherClass() { return new Foo2(); }
+    }
+
+    public class Foo2
+    {
+        private string SomeProp
+        {
+            get
+            {
+                return "dsds";
+            }
+        }
+    }
+}
---
+++ ReflectionMagicDemo/ReflectionMagicDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using ReflectionMagic;
+using ReflectionMagicLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionMagicDemo
+{
+    /// <summary>
+    /// ReflectionMagic     反射扩展库
+    /// Github:https://github.com/ReflectionMagic/ReflectionMagic
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom("ReflectionMagicLibrary.dll");
+            var fool1=assembly.CreateInstance("ReflectionMagicLibrary.Foo1");
+
+            string val = fool1.AsDynamic().GetOtherClass().SomeProp;
+
+            Console.ReadKey();
+        }
+    }
+}
