c103d115e8d6b80c668f895470221d44dd6fd71f
chenzhaojie <15298506403@163.com>
2020-07-19 11:06:53 +0800

    111

---
+++ DesignMode/DesignMode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignMode
+{
+    /// <summary>
+    /// 设计模式demo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 原型模式 https://www.cnblogs.com/weihanli/p/prototype-pattern.html
+            var resume = new SimpleResume();
+            resume.SetPersonalInfo("小明", "xiaoming@abc.xyz");
+            resume.SetWorkExperience("xxx公司", "1990~2000");
+            resume.Display();
+
+            var resume1 = (SimpleResume)resume.Clone();
+            resume1.SetWorkExperience("xxx企业", "1998~1999");
+            resume1.Display();
+
+            var resume2 = (SimpleResume)resume.Clone();
+            resume2.SetPersonalInfo("xiaohong", "xiaohong@abc.xyz");
+            resume2.Display();
+
+
+
+
+            var complexResume = new ComplexResume();
+            complexResume.SetPersonalInfo("xiaoming", "xiaoming@abc.xyz");
+            complexResume.SetWorkExperience("xiaomingTecch", "2001~2005");
+            complexResume.Show();
+
+            var complexResume1 = (ComplexResume)complexResume.Clone();
+            complexResume1.SetPersonalInfo("xiaohong", "xiaohong@abc.xyz");
+            complexResume1.Show();
+
+            #endregion
+        }
+    }
+}
---
+++ DesignMode/DesignMode/原型模式/SimpleResume.cs
@@ -0,0 +1,45 @@
+﻿/*
+*┌──────────────────────────────────────────┐
+*│  描述：SimpleResume                                   
+*│　作   者：chenzhaojie                                              
+*│　版   本：1.0                                              
+*│　创建时间：2020/7/19 10:58:02                        
+*└──────────────────────────────────────────┘
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignMode
+{
+    public class SimpleResume : ICloneable
+    {
+        private string _name;
+        private string _email;
+
+        private string _timePeriod;
+        private string _company;
+
+        public void SetPersonalInfo(string name, string email)
+        {
+            _name = name;
+            _email = email;
+        }
+
+        public void SetWorkExperience(string company, string timePeriod)
+        {
+            _company = company;
+            _timePeriod = timePeriod;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{_name} {_email}");
+            Console.WriteLine($"工作经历：{_timePeriod} {_company}");
+        }
+
+        public object Clone() => MemberwiseClone();
+    }
+}
