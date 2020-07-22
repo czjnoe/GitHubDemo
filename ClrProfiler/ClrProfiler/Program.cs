71dae5a5fd67aa48281aa145b11b30eca7b56c2b
15298 <15298@DESKTOP-US4MGUT>
2020-07-22 09:34:30 +0800

    888

--- ClrProfiler/ClrProfiler/Program.cs
+++ ClrProfiler/ClrProfiler/Program.cs
@@ -51,34 +51,35 @@ namespace ClrProfiler
             //string s1 = ("ggg");
             //s1 = ("gfg544");
 
-            //var Students = Enumerable.Range(0, 20 * 10000).Select(m => new Student()
-            //{
-            //    ID = m,
-            //    NAME = string.Intern(File.ReadLines(Environment.CurrentDirectory + "//TextFile1.txt")
-            //                    .ElementAt(m % 4))
-            //}).ToList();
-
-            List<string> nameList = new List<string>() { "WAP", "TAOBAO", "ETAO", "TMALL" };
-            using (IDbConnection db = DapperFactory.GetConnection(Enums.MyDbType.Oracle, @"User ID=czj;Password=123456;Data Source=(DESCRIPTION = (ADDRESS_LIST= (ADDRESS = (PROTOCOL = TCP)(HOST = 127.0.0.1)(PORT = 1521))) (CONNECT_DATA = (SERVICE_NAME = orcl)))"))
+            var Students = Enumerable.Range(0, 20 * 10000).Select(m => new Student()
             {
-                var list = db.Query<Student>("SELECT * FROM Student").ToList();
-                var wList=list.Where(w => w.ID < 1000);
-                var sList = list.Select(s => new Student { ID = s.ID, NAME = s.NAME }).ToList();
+                ID = m,
+                NAME = string.Intern(File.ReadLines(Environment.CurrentDirectory + "//TextFile1.txt")
+                                .ElementAt(m % 4))
+            }).ToList();
 
-                //var stuList = Enumerable.Range(0, 20 * 10000).Select(i => new Student()
-                //{
-                //    ID = i,
-                //    NAME = (nameList.ElementAt(i % 4))
-                //}).ToList();
-                //var result = db.Execute("insert into Student(ID,NAME) values(:ID,:NAME)", stuList);
-            }
+            //List<string> nameList = new List<string>() { "WAP", "TAOBAO", "ETAO", "TMALL" };
+            //using (IDbConnection db = DapperFactory.GetConnection(Enums.MyDbType.Oracle, @"User ID=czj;Password=123456;Data Source=(DESCRIPTION = (ADDRESS_LIST= (ADDRESS = (PROTOCOL = TCP)(HOST = 127.0.0.1)(PORT = 1521))) (CONNECT_DATA = (SERVICE_NAME = orcl)))"))
+            //{
+            //    var list = db.Query<Student>("SELECT * FROM Student").ToList();
 
-            string s1 = "22222222";
-            string str = string.Format("{0}", s1);
-            
+            //    //var wList=list.Where(w => w.ID < 1000);
+            //    //var sList = list.Select(s => new Student { ID = s.ID, NAME = s.NAME }).ToList();
+
+            //    //var stuList = Enumerable.Range(0, 20 * 10000).Select(i => new Student()
+            //    //{
+            //    //    ID = i,
+            //    //    NAME = (nameList.ElementAt(i % 4))
+            //    //}).ToList();
+            //    //var result = db.Execute("insert into Student(ID,NAME) values(:ID,:NAME)", stuList);
+            //}
+
+            //string s1 = "22222222";
+            //string str = string.Format("{0}", s1);
+
 
             GC.Collect();
-            Console.WriteLine("执行成功");
+            //Console.WriteLine("执行成功");
             Console.ReadLine();
         }
     }
