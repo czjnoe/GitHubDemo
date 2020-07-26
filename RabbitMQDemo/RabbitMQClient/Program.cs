75d46ebc0cc69b65254cf048bd85be28987e6171
15298 <15298@DESKTOP-US4MGUT>
2020-07-26 15:10:13 +0800

    2020-7-26

---
+++ RabbitMQDemo/RabbitMQClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQClient
+{
+    /// <summary>
+    /// RabbitMQ
+    /// 消息队列 生产者
+    /// 使用RabbitMQ.Client类库
+    /// GitHub:https://github.com/rabbitmq/rabbitmq-dotnet-client
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";//RabbitMQ服务地址
+            factory.UserName = "guest";//用户名
+            factory.Password = "guest";//密码
+            bool durable = true;//持久化
+
+            var queueName = "hello1";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queueName, durable, false, false, null);//创建一个名称为hello的消息队列
+                    string message = "Hello World"; //传递的消息内容
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.DeliveryMode = 2;
+                    properties.Persistent = true;//持久化  确保rabbitmq服务器重启不丢失数据
+
+
+                    channel.BasicPublish("", queueName, properties, body); //开始传递
+                    Console.WriteLine("已发送： {0}", message);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQDemo/RabbitMQServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQServer
+{
+    /// <summary>
+    /// 消息队列 消费者
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";//RabbitMQ服务地址
+            factory.UserName = "guest";//用户名
+            factory.Password = "guest";//密码
+
+            bool durable = true;//持久化
+            var queueName = "hello1";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queueName, durable, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                   
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine("已接收： {0}", message);
+
+                        channel.BasicAck(ea.DeliveryTag, false);//消息确认提交，会释放掉消息
+                    };
+                    channel.BasicConsume(queueName, false, consumer);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
